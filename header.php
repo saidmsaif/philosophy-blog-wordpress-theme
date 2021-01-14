3662b3e905e7e8a01353f9f6b2d44f4cc1f5d34b
Saifullah Saif <asmsaif15@gmail.com>
2021-01-14 22:25:19 +0600

    Reduce some code

--- header.php
+++ header.php
@@ -8,8 +8,6 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <!-- mobile specific metas
-    ================================================== -->
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
     <?php wp_head(); ?>
