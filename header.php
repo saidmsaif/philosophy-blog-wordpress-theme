aa914899cc9ad0fcf9f753e868b166ab61414ee9
Saifullah Saif <asmsaif15@gmail.com>
2021-01-14 21:31:48 +0600

    Remove assets files

--- header.php
+++ header.php
@@ -12,12 +12,6 @@
     ================================================== -->
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-    <!-- CSS
-    ================================================== -->
-    <link rel="stylesheet" href="css/base.css">
-    <link rel="stylesheet" href="css/vendor.css">
-    <link rel="stylesheet" href="css/main.css">
-
     <?php wp_head(); ?>
 </head>
 <body id="top">
